b8afa9f2f36cf4eb26b6842c343f786a9f4eeef8
pduyennn <89238911+pduyennn@users.noreply.github.com>
2021-09-17 21:23:18 +0700

    Initialize patch-2

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Xin chào Sơn xoăn iu dấu!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là iu em <33',
+    btnNo: 'Không, tình iu à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Em biết mà. Yêu anh nhìu nhìu 😘😘',
+    messDesc: 'Khi nào rảnh ùi thì nhớ kiu em mình đi ăng sinh nhật nheee.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/messages/t/100009076513841/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
